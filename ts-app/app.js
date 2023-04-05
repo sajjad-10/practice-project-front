c6034ff659ad2a81ef642c5dec6bf9a01f5a64c7
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-05 11:36:25 +0330

    core type

--- ts-app/app.js
+++ ts-app/app.js
@@ -1,1 +1,8 @@
-console.log("TS");
+console.log("TS App is running");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 6; // if like '6' get erroe
+var number2 = 4;
+var result = add(number1, number2);
+console.log(result);
--- ts-app/app.ts
+++ ts-app/app.ts
@@ -1,1 +1,11 @@
 console.log("TS App is running");
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+const number1 = 6; // if like '6' get error
+const number2 = 4;
+
+const result = add(number1, number2);
+
+console.log(result);
