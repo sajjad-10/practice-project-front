eaba9ea4f5384949f751093dd8730995b0e526a3
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-13 10:43:30 +0330

    data type - Reference - Object

--- Javascript Crash Course/script.js
+++ Javascript Crash Course/script.js
@@ -1,3 +1,19 @@
-// see print in dev-tools 
+// data type - Reference - Object
+// regular data
 
-console.log("Hello world");
+let name = "Sajjad",
+    age = 25,
+    admin = true;
+console.log(name, age, admin);
+
+// in object
+
+let person = {
+    name: "Sajjad",
+    age: 25,
+    admin: true,
+};
+
+console.log(person);
+console.log(person.sajjad); // dot notion
+console.log(person["age"]); // bracket notion
