d5f3e8ed056ec21351625d80f5a846c841ec37c3
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-15 07:04:38 +0330

    Clone object -Constructor

--- Javascript Crash Course/object/Clone_Object.js
+++ Javascript Crash Course/object/Clone_Object.js
@@ -6,9 +6,12 @@ const circle = {
 };
 
 /* 1 - use Loop */
-const another = {}
-for(let key in circle){
-    another[key] = circle[key]
+const another = {};
+for (let key in circle) {
+    another[key] = circle[key];
 }
 
-console.log(another);
+// console.log(another);
+
+const another2 = Object.assign({ color: "red" }, circle);
+console.log(another2);
