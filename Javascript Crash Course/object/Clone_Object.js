e9684c147ad35b0dd9928bb4773c132dfcecd654
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-15 07:09:32 +0330

    Clone object -Spread  Operator

--- Javascript Crash Course/object/Clone_Object.js
+++ Javascript Crash Course/object/Clone_Object.js
@@ -14,4 +14,7 @@ for (let key in circle) {
 // console.log(another);
 
 const another2 = Object.assign({ color: "red" }, circle);
-console.log(another2);
+// console.log(another2);
+
+const another3 = { location: 5, ...circle, color: "red" };
+console.log(another3);
