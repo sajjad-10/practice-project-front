b19c1b6337af4ba6481095062acfe0194faa5f39
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-08 07:42:44 +0330

    searching an element for  Primitive

--- Javascript Crash Course/array/searching-an-element.js
+++ Javascript Crash Course/array/searching-an-element.js
@@ -4,7 +4,6 @@ const numbers1 = [1, 2, 3, 1, 5, 4];
 console.log(numbers1.indexOf(1, 2)); /**find element position */
 console.log(numbers1.lastIndexOf(1, 2)); /**find element position */
 console.log(numbers1.includes(1, 2)); /**have element (boolean) */
-console.clear();
 
 // ->
 const obj1 = { key: "value" };
@@ -12,3 +11,20 @@ const obj2 = { key: "value" };
 const array1 = [obj1];
 console.log(array1.includes(obj1)); /** true */
 console.log(array1.includes(obj2)); /** true */
+console.clear();
+
+
+/* * for Primitive  */
+const array2 = [
+    { id: 1, name: "a" },
+    { id: 2, name: "b" },
+];
+
+const result1 = array2.find((item) => {
+    return item.name === "a";
+});
+const result2 = array2.findIndex((item) => {
+    return item.name === "a";
+});
+
+console.log(result1, "position :",result2);
