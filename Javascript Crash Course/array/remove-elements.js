55967ac1d31903bde57ff820fb5ce0fee03262a1
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-08 09:13:03 +0330

    remove elements

---
+++ Javascript Crash Course/array/remove-elements.js
@@ -0,0 +1,6 @@
+const numbers1 = [1, 2, 3, 4, 5];
+
+/** Remove Elements */
+const last = numbers1.pop(); /** return 5 */
+const first = numbers1.shift(); /** return 1 */
+const model = numbers1.splice(1, 2); /** return new array */
