5f55d6ea7c02ef6156c194f202389418ee7d1b28
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-10 15:11:46 +0330

    Tip for reference types -concatenates

--- Javascript Crash Course/array/new-array-from-array.js
+++ Javascript Crash Course/array/new-array-from-array.js
@@ -6,3 +6,12 @@ const combined = first1.concat(second1);
 
 /* New Array from old Array */
 const splice = combined.splice(1, 4);
+
+/* Tip for reference types */
+const first2 = [{ id: 1 }];
+const second2 = [4, 5, 6];
+
+first2[0].id =  99
+const combined2 = first2.concat(second2);
+console.log(first2); /* *return [ { id: 99 } ] */
+console.log(combined2);  /* *return [ { id: 99 }, 4, 5, 6 ] */
