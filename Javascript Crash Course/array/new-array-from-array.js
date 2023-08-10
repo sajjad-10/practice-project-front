673bb57a6520943e8a9ca00d6b62b15911a7dfab
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-10 14:40:08 +0330

    Concatenates & New array

---
+++ Javascript Crash Course/array/new-array-from-array.js
@@ -0,0 +1,8 @@
+/* concatenates (joins) */
+const first1 = [1, 2, 3];
+const second1 = [4, 5, 6];
+
+const combined = first1.concat(second1);
+
+/* New Array from old Array */
+const splice = combined.splice(1, 4);
