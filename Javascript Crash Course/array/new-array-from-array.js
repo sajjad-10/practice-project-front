4462cdc037e3843625242913eb734427603bb918
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-10 15:17:06 +0330

    Concatenates (joins) - Separate operator

--- Javascript Crash Course/array/new-array-from-array.js
+++ Javascript Crash Course/array/new-array-from-array.js
@@ -4,6 +4,9 @@ const second1 = [4, 5, 6];
 
 const combined = first1.concat(second1);
 
+/* concatenates (joins) - Separate operator */
+const combinedSeparate = ["a", ...first1, "b", ...second2, "c"];
+
 /* New Array from old Array */
 const splice = combined.splice(1, 4);
 
@@ -11,7 +14,7 @@ const splice = combined.splice(1, 4);
 const first2 = [{ id: 1 }];
 const second2 = [4, 5, 6];
 
-first2[0].id =  99
+first2[0].id = 99;
 const combined2 = first2.concat(second2);
 console.log(first2); /* *return [ { id: 99 } ] */
-console.log(combined2);  /* *return [ { id: 99 }, 4, 5, 6 ] */
+console.log(combined2); /* *return [ { id: 99 }, 4, 5, 6 ] */
