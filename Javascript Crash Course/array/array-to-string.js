d3ab374665ae9a04a20c3daa2a0ea2002b8348ce
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-11 12:25:23 +0330

    Array to String

---
+++ Javascript Crash Course/array/array-to-string.js
@@ -0,0 +1,8 @@
+/* Array to String */
+const numbers = [1, 2, 3, 4];
+
+const joined = numbers.join(" ");
+
+/* String to Array */
+const string = "JavaScript coding is cool";
+const parts = string.split(" ");
