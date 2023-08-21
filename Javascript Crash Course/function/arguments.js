71df102ce33dc064b6e2291737d5f25f213974f2
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-21 09:18:22 +0330

    …Rest Parameters

--- Javascript Crash Course/function/arguments.js
+++ Javascript Crash Course/function/arguments.js
@@ -5,4 +5,19 @@ function sum(a, b) {
     return total;
 }
 
-console.log(sum(1, 5, 9, 12, 18));
+// console.log(sum(1, 5, 9, 12, 18));
+
+/* ...Rest Parameters */
+function sum1(...args) {
+    return args.reduce((a, c) => a + c, 0);
+}
+
+// console.log(sum1(1, 5, 9, 12, 18));
+
+/* example2 ...Rest Parameters */
+function discount(discount, ...args) {
+    let total = args.reduce((a, c) => a + c, 0);
+    return total * (1 - discount);
+}
+
+console.log(sum1(0.5, 1, 5, 9, 12, 18));
